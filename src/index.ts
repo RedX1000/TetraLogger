ffb59ae4c2cb65c2ea107f8b54959c31331cf541
RedX1000 <clwms16@gmail.com>
2023-01-22 16:02:21 -0600

    Date/Time for history, Quantities for items, etc

--- src/index.ts
+++ src/index.ts
@@ -119,7 +119,7 @@ function tabChanger(){
 export async function init() {
 	buttonDisabler();
 
-	// TODO: This is a fix for when the buttons are clicked once.
+	// TODO: This is a fix for when the tab buttons are clicked once.
 	// When clicked once, it does nothing but when clicked a second
 	// time, it closes and works properly.
 	// Figure out in toggleLootDisplay how to fix it. Might worry
@@ -189,6 +189,20 @@ export async function init() {
 	if (localStorage.getItem("TetraLogger/History") == null) { // History initializer
 		if (seeConsoleLogs) console.log("Creating history");
 		localStorage.setItem("TetraLogger/History",JSON.stringify([]));
+	}
+
+
+	// This code should add the current date to your history log if it does not exist.
+	// This snippet can be removed a few months in the future or for future projects with this code.
+	// ~ 11/21/2022
+	let history = JSON.parse(localStorage.getItem("TetraLogger/History"))
+	if(history != null){
+		for(let i = 0; i < history.length; i++){
+			if(history[i][6] == undefined){
+				history[i].push(await dateGetter())
+			}
+		}
+		localStorage.setItem("TetraLogger/History",JSON.stringify(history))
 	}
 
 	
@@ -788,7 +802,7 @@ async function findtrailComplete(img: ImgRef, autobool: boolean) {
 				let quantvar = document.createElement("span") as HTMLSpanElement;
 
 				nodevar = nodeMaker(parseInt(quantResults[(i * 8) + j]), itemResults[i][j], "recent")
-				imgvar = imgMaker(itemResults[i][j])
+				imgvar = imgMaker(itemResults[i][j], parseInt(quantResults[(i * 8) + j]))
 				quantvar = quantMaker(parseInt(quantResults[(i * 8) + j]))
 
 				nodevar.append(quantvar);
@@ -1095,12 +1109,13 @@ async function submitToLS(item: any[], quant: any[], value: any) {
 
 async function addHistoryToLs(value: number, item: any, quants: any, reward: any) {
 	// The order of how History items are logged
-	// Index 1: Items (Array)
-	// Index 2: Quantities (Array)
-	// Index 3: Value
-	// Index 4: "Reward" or "Reward [C] "
-	// Index 5: reward count
-	// Index 6: History Primary Key
+	// Index 0: Items (Array)
+	// Index 1: Quantities (Array)
+	// Index 2: Value
+	// Index 3: "Reward" or "Reward [C] "
+	// Index 4: reward count
+	// Index 5: History Primary Key
+	// Index 6: Date and time captured
 	let itemsArr = []
 	for (let i = 0; i < item.length; i++) {
 		for (let j = 0; j < item[i].length; j++) {
@@ -1117,8 +1132,10 @@ async function addHistoryToLs(value: number, item: any, quants: any, reward: any
 			quants[i] += "000";
 		}
 	}
+
+	let currentDateTime = await dateGetter()
 	
-	let previous = [itemsArr, quants, value, reward, localStorage.getItem("TetraLogger/Count"), localStorage.getItem("TetraLogger/PrimaryKeyHistory")];
+	let previous = [itemsArr, quants, value, reward, localStorage.getItem("TetraLogger/Count"), localStorage.getItem("TetraLogger/PrimaryKeyHistory"), currentDateTime];
 	let temp = JSON.parse(localStorage.getItem("TetraLogger/History"))
 	temp.push(previous);
 
@@ -1153,7 +1170,7 @@ function tabDisplay() {
 		divs[i].textContent = "";
 	}
 	for (let i = 0; i < keys.length; i++) {
-		// Interesting tidbit: Comment out this if block to display every item, 
+		// TODO: Interesting tidbit: Comment out this if block to display every item, 
 		// but quantities will be undefined for the given tier if it doesn't exist in it.
 		if (items[keys[i]].quantity == undefined || items[keys[i]].quantity == 0) {
 			continue;
@@ -1167,15 +1184,16 @@ function tabDisplay() {
 
 		nodevar = nodeMaker(parseInt(items[keys[i]].quantity), keys[i], "tab");
 		nodevar.style.order = orderChecker(parseInt(items[keys[i]].order), keys[i]).toString();
-		imgvar = imgMaker(keys[i]);
 		
 		// This if else only exists for when I comment out the above if block.
 		// Nice for viewing all of the loot.
 		if (items[keys[i]].quantity == undefined) {
 			quantvar = quantMaker(0);
+			imgvar = imgMaker(keys[i], 0);
 		}
 		else {
 			quantvar = quantMaker(items[keys[i]].quantity);
+			imgvar = imgMaker(keys[i], items[keys[i]].quantity);
 		}
 
 		nodevar.append(quantvar);
@@ -1215,6 +1233,16 @@ function historyInit() {
 				container.setAttribute("class", "historyDisplayContainer");
 				container.setAttribute('id','container' + temp[5]);
 
+				let dateBox = document.createElement("div") as HTMLDivElement;
+				let dateImg = document.createElement("div") as HTMLDivElement;
+				
+				dateBox.setAttribute('class', 'dateBox')
+				dateImg.setAttribute('class', 'dateImage')
+				dateImg.setAttribute('title', 'Date Captured: ' + temp[6])
+			
+				dateBox.append(dateImg)
+				container.append(dateBox)
+
 				if (temp[3].includes(" [C] ")) {
 					let customSpan = document.createElement("span") as HTMLSpanElement;
 					customSpan.setAttribute("class", "customSpan");
@@ -1252,7 +1280,7 @@ function historyInit() {
 								let imgvar = document.createElement("img") as HTMLImageElement;
 								let quantvar = document.createElement("span") as HTMLSpanElement;
 
-								imgvar = imgMaker("Transparent");
+								imgvar = imgMaker("Transparent", temp[1][(j * 8) + k]);
 								nodevar.setAttribute("class", "node_history");
 								nodevar.removeAttribute("title");
 								quantvar.textContent = "";
@@ -1272,13 +1300,13 @@ function historyInit() {
 						// Note for later. Figure out why insert isnt displaying properly...
 			
 						if (temp[1][(j * 8) + k] === undefined) {
-							imgvar = imgMaker("Transparent");
+							imgvar = imgMaker("Transparent", temp[1][(j * 8) + k]);
 							nodevar.setAttribute("class", "node_history");
 							nodevar.removeAttribute("title");
 							quantvar.textContent = "";
 						}
 						else {
-							imgvar = imgMaker(temp[0][(j * 8) + k]);
+							imgvar = imgMaker(temp[0][(j * 8) + k], temp[1][(j * 8) + k]);
 							nodevar = nodeMaker(parseInt(temp[1][(j * 8) + k]), temp[0][(j * 8) + k], "history");
 							quantvar = quantMaker(temp[1][(j * 8) + k]);
 						}
@@ -1547,7 +1575,7 @@ export async function fetchFromGE() {
 }
 
 
-export function verifyInsert(event: Event) {
+export async function verifyInsert(event: Event) {
 	if (seeConsoleLogs) console.log("Collecting info from insert...");
 	let itemsList = [];
 	let quants = [];
@@ -1590,6 +1618,16 @@ export function verifyInsert(event: Event) {
 	container.setAttribute("class", 'historyDisplayContainer');
 	container.setAttribute('id','container' + curr);
 
+	let dateBox = document.createElement("div") as HTMLDivElement;
+	let dateImg = document.createElement("div") as HTMLDivElement;
+	
+	dateBox.setAttribute('class', 'dateBox')
+	dateImg.setAttribute('class', 'dateImage')
+	dateImg.setAttribute('title', 'Date Captured: ' + (await dateGetter()))
+
+	dateBox.append(dateImg)
+	container.append(dateBox)
+
 	let customSpan = document.createElement("span") as HTMLSpanElement;
 	customSpan.setAttribute("class", "customSpan");
 	customSpan.setAttribute("title", "Custom clue manually inserted.");
@@ -1619,7 +1657,7 @@ export function verifyInsert(event: Event) {
 					let imgvar = document.createElement("img") as HTMLImageElement;
 					let quantvar = document.createElement("span") as HTMLSpanElement;
 
-					imgvar = imgMaker("Transparent");
+					imgvar = imgMaker("Transparent", quants[(j * 8) + k]);
 					nodevar.setAttribute("class", "node_history");
 					nodevar.removeAttribute("title");
 					quantvar.textContent = "";
@@ -1639,13 +1677,13 @@ export function verifyInsert(event: Event) {
 			// Note for later. Figure out why insert isnt displaying properly...
 
 			if (quants[(j * 8) + k] === undefined) {
-				imgvar = imgMaker("Transparent");
+				imgvar = imgMaker("Transparent", quants[(j * 8) + k]);
 				nodevar.setAttribute("class", "node_history");
 				nodevar.removeAttribute("title");
 				quantvar.textContent = "";
 			}
 			else {
-				imgvar = imgMaker(itemsList[(j * 8) + k]);
+				imgvar = imgMaker(itemsList[(j * 8) + k], quants[(j * 8) + k]);
 				nodevar = nodeMaker(parseInt(quants[(j * 8) + k]), itemsList[(j * 8) + k], "history");
 				quantvar = quantMaker(quants[(j * 8) + k]);
 			}
@@ -1970,13 +2008,13 @@ export function exporttocsv() {
 	}
 	csvinfo.push([])
 	csvinfo.push([])
-	csvinfo.push(["Captured Rewards History", 'Parse tier at " : " and " [C] "','Parse items at " x "'])
-	csvinfo.push(["Rewards Tier & Count", "Reward Value", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21"])
+	csvinfo.push(["Captured Rewards History", 'Parse tier at " : " and " [C] "', '"Parse date and time at "", " "', 'Parse items at " x "'])
+	csvinfo.push(["Rewards Tier & Count", "Reward Value", "Date and Time recorded", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21"])
 	console.log(lsHistory)
 	if (seeConsoleLogs) console.log("Setting history in csv...")
 	for (let i = 0; i < lsHistory.length; i++) {
 		lsHistory[i][4] = i + 1
-		let temp = [lsHistory[i][3] + " : " + lsHistory[i][4], lsHistory[i][2]]
+		let temp = [lsHistory[i][3] + " : " + lsHistory[i][4], lsHistory[i][2], '"' + lsHistory[i][6].toString() + '"']
 		for (let j = 0; j < 4; j++) {
 			for(let k = 0; k < 8; k++){
 				if(lsHistory[i][0][(j * 8) + k] == undefined || lsHistory[i][0][(j * 8) + k] === "Blank"){
@@ -1989,15 +2027,21 @@ export function exporttocsv() {
 		}
 		csvinfo.push(temp)
 	}
+	localStorage.setItem("TetraLogger/History", JSON.stringify(lsHistory))
 
 	const d = new Date();
+	let hour = "0" + d.getHours().toString()
+	let minute = "0" + d.getMinutes().toString()
+	let second = "0" + d.getSeconds().toString()
+	let month = "0" + (d.getMonth() + 1).toString()
+	let day = "0" + d.getDate().toString()
 	let csvContent = "";
 	csvinfo.forEach(function (i) {
 		let row = i.join(",");
 		csvContent += row + "\r\n";
 	});
 
-	let filename = "TetraLogger CSV " + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + "_" + d.getHours() + "-" + d.getMinutes() + "-" +d.getSeconds()+ ".csv";
+	let filename = "TetraLogger CSV " + (d.getFullYear() + "-" + month.slice(-2) + "-" + day.slice(-2) + "--" + hour.slice(-2) + "-" + minute.slice(-2) + "-" + second.slice(-2)) + ".csv";
 	let encodedUri = "data:text/csv;charset=utf-8,%EF%BB%BF" + encodeURI(csvContent);
 	let link = document.createElement("a") as HTMLAnchorElement;
 	link.setAttribute("href", encodedUri);
@@ -2030,9 +2074,18 @@ function nodeMaker(quant: number, item: string, attribute:string) {
 }
 
 
-function imgMaker(item: string) {
+function imgMaker(item: string, quant:number) {
 	let imgvar = document.createElement("img") as HTMLImageElement;
-	imgvar.src = encodeURI("./images/items/" + item.replace("/","-") + ".png");
+	
+	if(["Golden dragonfruit seed", "Carambola seed"].includes(item)){
+		if(quant == 3)
+			imgvar.src = encodeURI("./images/items/" + item.replace(" ", "_").replace(" ", "_") + "_3.png");
+		else
+			imgvar.src = encodeURI("./images/items/" + item + ".png");
+	}
+	else
+		imgvar.src = encodeURI("./images/items/" + item.replace("/","-") + ".png");
+	
 	imgvar.setAttribute('style', 'margin:auto;');
 	imgvar.ondragstart = function() { return false; };
 	return imgvar
@@ -2055,6 +2108,18 @@ function quantMaker(quant: number) {
 		quantvar.textContent = quant + "";
 	}
 	return quantvar
+}
+
+
+async function dateGetter(){
+	const d = new Date();
+	let hour = "0" + d.getUTCHours().toString()
+	let minute = "0" + d.getUTCMinutes().toString()
+	let second = "0" + d.getUTCSeconds().toString()
+	let month = "0" + (d.getUTCMonth() + 1).toString()
+	let day = "0" + d.getUTCDate().toString()
+	let currentDate = hour.slice(-2) + ":" + minute.slice(-2) + ":" + second.slice(-2) + ", " + d.getUTCFullYear() + "/" + month.slice(-2) + "/" + day.slice(-2) + " UTC"
+	return currentDate
 }
 
 
@@ -2127,22 +2192,25 @@ export function toggleLootDisplay(id: string) {
 		minH = 75;
 	}
 
+	let minHval = (minH + "%").toString()
+	minHval = "80px"
+
 	if (opentabs[0]) {
-		Array.from(document.getElementsByClassName('artifacts') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minH + "%";
+		Array.from(document.getElementsByClassName('artifacts') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minHval;
 	}
 	else {
 		Array.from(document.getElementsByClassName('artifacts') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = "8%";
 	}
 
 	if (opentabs[1]) {
-		Array.from(document.getElementsByClassName('materials') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minH + "%";
+		Array.from(document.getElementsByClassName('materials') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minHval;
 	}
 	else {
 		Array.from(document.getElementsByClassName('materials') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = "8%";
 	}
 
 	if (opentabs[2]) {
-		Array.from(document.getElementsByClassName('miscellaneous') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minH + "%";
+		Array.from(document.getElementsByClassName('miscellaneous') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = minHval;
 	}
 	else {
 		Array.from(document.getElementsByClassName('miscellaneous') as HTMLCollectionOf<HTMLElement>)[0].style.minHeight = "8%";
